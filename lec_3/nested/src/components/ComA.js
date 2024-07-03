2b01f519772d299f2c08aefb50814a987f7b3730
krish ranghani <krishranghani@gmail.com>
2024-07-03 11:45:34 +0530

    3/7/2024

---
+++ lec_3/nested/src/components/ComA.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ComB from './ComB'
+
+function ComA() {
+  return (
+    <div>
+      <ComB/>
+    </div>
+  )
+}
+
+
+export default ComA
---
+++ lec_3/nested/src/components/ComC.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ComC() {
+  return (
+    <div>
+      <h1> i an components c</h1>
+    </div>
+  )
+}
+
+export default ComC;
